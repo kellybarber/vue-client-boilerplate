03d4830a4c79bc97d1d01ac951655c240692a931
kellybarber <kelly.james.barber@gmail.com>
2018-08-20 09:05:36 -0400

    Added deployment server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = process.env.PORT || 8080
+const publicPath = path.join(__dirname, '..', 'dist')
+
+app.use(express.static(publicPath))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'))
+})
+
+app.listen(port, () => console.log(`Server is running on port ${port}` ))
