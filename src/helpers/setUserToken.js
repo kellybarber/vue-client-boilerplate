b7dc8f2e56f4392a669ed24d4a23f3b343320d68
kellybarber <kelly.james.barber@gmail.com>
2018-08-21 11:31:09 -0400

    Implicit return on token setup function

--- src/helpers/setUserToken.js
+++ src/helpers/setUserToken.js
@@ -1,7 +1,5 @@
-export default () => {
-  return { 
-    auth: { 
-      authenticated: localStorage.getItem('token') 
-    }
+export default () => ({ 
+  auth: { 
+    authenticated: localStorage.getItem('token') 
   }
-}
+})
