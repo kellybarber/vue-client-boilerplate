5c8992e169a728ced8562cd7add6dbaa1b8926d3
Kelly Barber <kelly.james.barber@gmail.com>
2018-09-20 14:55:35 -0400

    Created base components for app layout

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Header from '../Header/Header'
+import RepoContainer from '../RepoContainer/RepoContainer'
 import { background } from './Dashboard.css'
 
 export default () => (
   <div className={background}>
-    
+    <RepoContainer/>
   </div>
 )
---
+++ src/components/RepoContainer/RepoContainer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { container } from './RepoContainer.css'
+
+export default () => (
+  <div className={container}>
+    
+  </div>
+)
