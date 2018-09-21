015d63b16493925f15c876027f0ce178cacbd848
Kelly Barber <kelly.james.barber@gmail.com>
2018-09-21 10:58:01 -0400

    Conditional rendering for the table and table list is working when the search bar is cleared or the search is submitted

---
+++ src/components/RepoListItem/RepoListItem.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class RepoListItem extends Component {
+  render() {
+    console.log(this.props)
+    
+
+    return (
+      <tr>
+
+      </tr>
+    )
+  }
+}
+
+export default RepoListItem
--- src/components/RepoSearchList/RepoSearchList.js
+++ src/components/RepoSearchList/RepoSearchList.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { table } from './RepoSearchList.css'
 
+import RepoListItem from '../RepoListItem/RepoListItem'
+ 
 class RepoSearchList extends Component {
   render() {
     const { repos } = this.props
     const show = Object.keys(repos)[0] !== undefined
+    // const show = true
 
     if (show) {
       return (
-        <table>
+        <table className={table}>
           <thead>
             <tr>
               <th>Name</th>
               <th>Language</th>
               <th>Latest Tag</th>
+              <th>Favourite</th>
             </tr>
           </thead>
+          <tbody>
+            {repos.repositories.map(repo => {
+              return <RepoListItem key={repo.id} {...repo}/>
+            })}
+          </tbody>
         </table>
       )
     } else {
